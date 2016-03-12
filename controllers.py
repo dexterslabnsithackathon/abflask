5f9f4b323909690ef3543e8e14786624b8bce647
MohitS10 <iammohitsharma@gmail.com>
2016-03-13 00:47:51 +0530

    added session cookie for variants

--- controllers.py
+++ controllers.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, make_response
+from flask import Flask, render_template, request, redirect, make_response, session, escape
 import os
 from pymongo import MongoClient
 # from pymongo import mongo
@@ -11,23 +11,37 @@ db = client['admin']
 def index():
     f = db.abtestdata.find();
     for x in f:
-        # if x['route']=='index':
-            # assumes all users are new. use cookies for old user
-            # update flag, add total, create cookie, update db and redirect
-        flag=x['flag']
-        x['total'][flag] = x['total'][flag] + 1
-        flag = (flag+1)%x['num'] 
-        #create cookie
-        db.abtestdata.update(
-            { 'route': 'index' },
-            { '$set': 
-                {
-                    'flag':flag,
-                    'total':x['total']
-                }
-            }
-        )
-        return render_template(x['variant'][x['flag']]+'.html')
+        if x['route']=='index':
+            #old user
+            if 'index' in session:
+                flag=session['index']
+                x['total'][flag] = x['total'][flag] + 1 
+                db.abtestdata.update(
+                    { 'route': 'index' },
+                    { '$set': 
+                        {
+                            'flag':flag,
+                            'total':x['total']
+                        }
+                    }
+                )
+                return render_template(x['variant'][x['flag']]+'.html')
+            else:    
+            # new user
+                flag=x['flag']
+                x['total'][flag] = x['total'][flag] + 1
+                flag = (flag+1)%x['num'] 
+                session['index']=x['flag']
+                db.abtestdata.update(
+                    { 'route': 'index' },
+                    { '$set': 
+                        {
+                            'flag':flag,
+                            'total':x['total']
+                        }
+                    }
+                )
+                return render_template(x['variant'][x['flag']]+'.html')
         break
     # return "How did I get here? 404"
     return "Old Index Page without Variants!"
@@ -64,6 +78,9 @@ def success(var):
 def result():
     f = db.abtestdata.find();
     return render_template('result.html',f=f)
+
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 if __name__ == "__main__":
     app.run(debug=True)
